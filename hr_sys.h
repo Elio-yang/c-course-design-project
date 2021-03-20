449d7306cfa6635de7a20fee0e5d03b74f57f7a6
elio <2925539854@qq.com>
2021-03-21 00:28:39 +0800

    Yangyang 2021/03/21 hr_sys version2

--- hr_sys.h
+++ hr_sys.h
@@ -371,6 +371,8 @@ void _insert_worker(Staff *staff)
 
 void insert_worker()
 {
+        Staff *staff=(Staff*)malloc(sizeof(*staff));
+
         char name[32];
         char hire_time[32];
         char gen[10];
@@ -381,40 +383,119 @@ void insert_worker()
         char wid[15];
         char salary[15];
 
-        printf("Please enter new worker infomation:\n");
+        //TODO finish strict check way && improve input parser && exit way
+        printf("Please enter new worker information:\n");
         printf("Name      > ");
         scanf("%s",name);
 
         printf("Hire time > ");
         scanf("%s",hire_time);
-
+        GENDER:
         printf("Gender    > ");
         scanf("%s",gen);
 
+        if(strcmp(gen,"MALE")==0){
+                gender=MALE;
+        }
+        else if(strcmp(gen,"FEMALE")==0){
+                gender=FEMALE;
+        }
+        else{
+                fprintf(stderr,"Gender error please re-input.\n");
+                goto GENDER;
+        }
+
+        RANK:
         printf("Rank      > ");
         scanf("%s",ran);
 
+        if (strcmp(ran, "BOSS") == 0) {
+                staff->rank = BOSS;
+        } else if (strcmp(ran, "MANAGER") == 0) {
+                staff->rank = MANAGER;
+        } else if (strcmp(ran, "BARTENDER") == 0) {
+                staff->rank = BARTENDER;
+        } else if (strcmp(ran, "COOK") == 0) {
+                staff->rank = COOK;
+        } else if (strcmp(ran, "CLEANER") == 0) {
+                staff->rank = CLEANER;
+        } else if (strcmp(ran, "CASHIER") == 0) {
+                staff->rank = CASHIER;
+        } else if (strcmp(ran, "WAREHOUSEMAN") == 0) {
+                staff->rank = WAREHOUSEMAN;
+        } else if (strcmp(ran, "FINANCE") == 0) {
+                staff->rank = FINANCE;
+        } else {
+                fprintf(stderr, "rank error please re-input.\n");
+                goto RANK;
+        }
+        PID:
         printf("Pid       > ");
         scanf("%s",pid);
+        if(strlen(pid)!=14 || strncmp(pid,"4023",4)!=0){
+                fprintf(stderr,"pid error please re-input.\n");
+                goto PID;
+        }
+
 
         printf("Wid       > ");
         scanf("%s",wid);
+        if(strlen(wid)!= 6 || atoi(wid)<=0){
+                fprintf(stderr,"pid error please re-input.\n");
+                goto PID;
+        }
 
+        //TODO CHECK
         printf("Salary    > ");
         scanf("%s",salary);
 
-        Staff *staff=(Staff*)malloc(sizeof(*staff));
+
         strcpy(staff->name,name);
         strcpy(staff->hire_time,hire_time);
         staff->gender=gender;
-        staff->rank=rank;
         strcpy(staff->pid,pid);
         strcpy(staff->wid,wid);
         strcpy(staff->salary,salary);
         _insert_worker(staff);
 }
 
-void remove_worker();
+void _remove_worker(Staff *staff)
+{
+        //TODO change to double link list
+        Staff *head = get_head(HR_LIST);
+
+        Staff *prev=NULL;
+        while(head->next!=NULL){
+                if(strcmp(head->next->wid,staff->wid)==0) {
+                        prev=head;
+                        break;
+                }
+        }
+        prev->next=staff->next;
+        staff->next=NULL;
+        if(staff->recd_list!=NULL){
+                //TODO recdlist free
+                free(staff->recd_list);
+        }
+        free(staff);
+        --HR_LIST->cnt;
+}
+bool remove_worker(const char *wid)
+{
+        if(HR_LIST->cnt==0 ||
+                (HR_LIST->cnt==1 && strcmp(wid,HR_LIST->head->next->wid)!=0)){
+                fprintf(stderr,"Remove error no worker.\n");
+                return false;
+        }
+        Staff *this = query_by_wid(wid);
+        if(this!=NULL){
+                _remove_worker(this);
+                return true;
+        }else{
+                printf("No such worker with wid : %s\n",wid);
+                return false;
+        }
+}
 
 
 void flush_disk()
@@ -459,6 +540,30 @@ void flush_disk()
         HR_LIST->fd = -1;
         close(fd);
 }
+
+void free_hr_list()
+{
+        Staff *ptr =HR_LIST->head;
+
+        while(ptr->next!=NULL){
+                Staff *staff = ptr->next;
+                _remove_worker(staff);
+        }
+}
+
+// called when exit the whole sys
+void exit_hr_sys()
+{
+        flush_disk();
+        free_hr_list();
+        return;
+}
+
+void sync_hr_sys()
+{
+        flush_disk();
+}
+
 void add_complaint(Complaint_record *recd);
 
 #endif
