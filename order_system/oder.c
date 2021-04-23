1f4c1d9505e87e0426e3729a64580b25304dadac
jakiejoe <56106412+wangjie200@users.noreply.github.com>
2021-04-23 20:51:15 +0800

    4.23 firstversion

--- order_system/oder.c
+++ order_system/oder.c
@@ -4,6 +4,53 @@ int a;
 dish menu[200];
 order *order_head; //the head of the order;
 
+void get_menu()
+{
+    FILE *p;
+    if (p = fopen("C:\\cprogramming\\C-Course-Design\\menu.TXT", "r") == NULL)
+        {
+            printf("fail to get the menu\n");
+            return;
+        }
+    else
+        while (!feof(p))
+        {
+            fscanf(p, "%[^,],%f,%d", menu[dishes_number].name, &menu[dishes_number].price, &menu[dishes_number].personalization_availble);
+            //menu[dishes_number].available_number = warehouse_check(menu[dishes_number].name);
+            for(int i=0;i<menu[dishes_number].personalization_availble;i++)                                           //regular expression
+                fscanf(p,"%[^s]",menu[dishes_number].personalization[i]);
+            dishes_number++;
+        }
+}
+void print_menu()
+{
+    for(int i=0;i<dishes_number;i++)
+        printf("%d.%s %d\n",i+1,menu[i].name,menu[i].price);
+}
+
+void print_order()
+{
+    int aggregate_price=0;
+    order *p = order_head;
+    while (p != NULL) //print the order
+    {
+        printf("%d:%s %d %s %d\n", p->index, p->name, p->number, p->personlization, p->price);
+        aggregate_price+=p->price;
+        p = p->next;
+    }
+    printf("the aggregate price is %d\n",aggregate_price);
+}
+
+void print_order_interface()
+{
+    printf("*********************instructions*********************\n"
+           "*you can place an order using the following command: *\n"
+           "*a:add a dishes                                      *\n"
+           "*c:check your order and change it                    *\n"
+           "*q:quit the order interface directly                 *\n"
+           "*f:check confirm and finish your ordering            *\n");
+}
+
 int delete_dishes() //success return 1,or return 0
 {
     char name[50];
@@ -73,16 +120,6 @@ int change_order()
         return 1;
     }
 }
-void print_order_interface()
-{
-    //get_menu();
-    printf("*********************instructions*********************\n"
-           "*you can place an order using the following command: *\n"
-           "*a:add a dishes                                      *\n"
-           "*c:check your order and change it                    *\n"
-           "*q:quit the order interface directly                 *\n"
-           "*f:check confirm and finish your ordering            *\n");
-}
 
 int add_dishes() //需要设计输入，考虑情况
 {
@@ -113,15 +150,15 @@ int add_dishes() //需要设计输入，考虑情况
             {
                 char choice[20];
                 unsigned int i;
-                scanf("%s", choice);
-
+                gets(choice);
+                
                 for (i = 0; i < menu[index - 1].personalization_availble; i++)
                     if (strcmp(menu[index - 1].personalization[i], choice) == 0)
                         break;
                 if (i < menu[index - 1].personalization_availble)
                 {
                     strcat(taste, choice);
-
+                    strcat(taste, '\s');
                     char insturcution;
                     printf("continue(c) or quit(q)?\n");
                     scanf("%c", &insturcution);
@@ -147,7 +184,7 @@ int add_dishes() //需要设计输入，考虑情况
     strcpy(q->name, menu[index - 1].name);
     q->number = number;
     q->price = menu[index - 1].price;
-    strcpy(q->personlization[], taste);
+    memcpy(q->personlization[0], taste,strlen(taste));            //copy the taste choice to the order
 
     while (p->next != NULL) //if p isn't the last one, then move afterwars
         p = p->next;
@@ -158,13 +195,12 @@ int add_dishes() //需要设计输入，考虑情况
 
 void check()
 {
-    order *p = order_head;
-    while (p != NULL) //print the order
-    {
-        printf("%d:%s %d %s %d\n", p->index, p->name, p->number, p->personlization, p->price);
-        p = p->next;
-    }
+    print_order();
+    change();
+}
 
+void change()        //check or finish?
+{
     char instruction;
     printf("you can change or delete dishes or quit by 'c'(change) or 'd'(delete) or 'q'(quit) command\n");
     while (1)
@@ -187,39 +223,36 @@ void check()
         }
     }
 }
-void get_menu()
-{
-    FILE *p;
-    if (p = fopen("C:\\cprogramming\\C-Course-Design\\menu.TXT", "r") == NULL)
-        {
-            printf("fail to get the menu\n");
-            return 0;
-        }
-    else
-        while (!feof(p))
-        {
-            fscanf(p, "%[^,],%f,%d", menu[dishes_number].name, &menu[dishes_number].price, &menu[dishes_number].personalization_availble);
-            menu[dishes_number].available_number = warehouse_check(menu[dishes_number].name);
-            for(int i=0;i<menu[dishes_number].personalization_availble;i++)                                           //regular expression
-                fscanf(p,"%[^\s]",menu[dishes_number].personalization[i]);
-            printf("%s %.2f",menu[dishes_number].name,menu[dishes_number].price);
-            dishes_number++;
-        }
-}
 
 int finish()
 {
-    order *p = order_head;
-    while (p != NULL) //print the order
+    char instruction;
+    print_order();
+    printf("are you going to confirm(q) your order or change(c) it?\n");
+    while(1)
     {
-        printf("%d:%s %d %s %d\n", p->index, p->name, p->number, p->personlization, p->price);
-        p = p->next;
+        scanf("%c",&instruction);
+        switch (instruction)
+        {
+        case 'q':
+            return 1;
+            break;
+        case 'c':
+            change();
+            break;
+        default:
+            printf("instuction doesn't exist\n"
+                    "please input again\n");
+            break;
+        }
     }
-    return 1;
 }
+
 
 int main(void)
 {
+    get_menu();
+    print_menu();
     print_order_interface();
     order_head = (order *)malloc(sizeof(order));
     order_head->next = NULL;
@@ -227,7 +260,7 @@ int main(void)
     char instruction;
     while (1)
     {
-        scanf("%d", &instruction);
+        scanf("%c", &instruction);
         switch (instruction)
         {
         case 'a':
@@ -238,13 +271,10 @@ int main(void)
             break;
         case 'f':              //check,confrim the order and finish
             if (finish() == 1) //if user decides to finish then update the ware house and quit
-            {
-                warehouse_update(order_head);
                 return 1;
-            }
             break;
         case 'q':
-            return 0; //quit the order interface directly
+            return 0;           //quit the order interface directly
             break;
         default:
             printf("instuction doesn't exist\n");
